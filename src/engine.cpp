0bfe0fb23b32dee8491094193c5574b4a416180f
Daniel Schnabel <daniel@solarwinds.cloud>
2020-11-07 20:40:04 -0800

    parse and use speech marks

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,22 +1,24 @@
 #include "Expression.hpp"
+#include "json.hpp"
 
 #include <string.h>
 #include <map>
 
 using namespace std;
+using json = nlohmann::json;
 
-void nextAnimation(map<int, string> &timing, string &mp3Path) {
+void nextAnimation(map<int, string> &marks, string &mp3Path) {
     ExpressionManager &manager = ExpressionManager::getInstance();
     manager.pauseBlink(true);
 
     // init structure
     viseme_timing_t vt = (viseme_timing_t){PTHREAD_MUTEX_INITIALIZER, PTHREAD_COND_INITIALIZER, 0, 0, NULL};
-    vt.timing_size = timing.size();
+    vt.timing_size = marks.size();
     vt.timing = (int *)calloc(vt.timing_size, sizeof(int));
 
     // copy each timing to the structure array
     int i = 0;
-    for (auto t : timing) vt.timing[i++] = t.first;
+    for (auto t : marks) vt.timing[i++] = t.first;
 
     // start playing audio in a separate thread
     thread play_audio(ad_play_audio_file, ad_wait_ready(), mp3Path.c_str(), 0.7, &vt);
@@ -29,7 +31,7 @@ void nextAnimation(map<int, string> &timing, string &mp3Path) {
 
         if (vt.next_timing <= vt.timing_size) {
             // TODO put transition expression here
-            cout << "timing: " << timing[vt.timing[vt.next_timing-1]] << endl;
+            cout << "timing: " << marks[vt.timing[vt.next_timing-1]] << endl;
         }
     }
 
@@ -48,24 +50,22 @@ int main(int argc, char **argv) {
 
     manager.transition(HAPPY, true);
 
-    //////////////
-    map<int, string> t;
-    t[0] = "60";t[1000] = "59";t[2000] = "58";t[3000] = "57";t[4000] = "56";t[5000] = "55";
-    t[6000] = "54";t[7000] = "53";t[8000] = "52";t[9000] = "51";t[10000] = "50";
-    t[11000] = "49";t[12000] = "48";t[13000] = "47";t[14000] = "46";t[15000] = "45";
-    t[16000] = "44";t[17000] = "43";t[18000] = "42";t[19000] = "41";t[20000] = "40";
-    t[25000] = "35";t[30000] = "30";t[35000] = "25";t[40000] = "20";t[45000] = "15";
-    t[50000] = "10";t[55000] = "05";t[59000] = "01";t[60000] = "00";
-    t[11500] = "49.5";t[12500] = "48.5";t[13500] = "47.5";
+    ifstream i("../media/speech.marks");
+    string str;
 
-    string p = "../media/test2.mp3";
-    //////////////
-
-    while (1) {
-        thread animate(nextAnimation, ref(t), ref(p));
-        animate.detach();
-        sleep(15);
+    json speechMarks;
+    while(getline(i, str)) {
+        speechMarks["marks"].push_back(json::parse(str));
     }
+
+    map<int, string> marks;
+    for (json::iterator it = speechMarks["marks"].begin(); it != speechMarks["marks"].end(); ++it) {
+      marks[(*it)["time"]] = (*it)["value"];
+    }
+
+    string speech = "../media/24000Hz.mp3";
+    thread animate(nextAnimation, ref(marks), ref(speech));
+    animate.detach();
 
     while (1) sleep(5);
 }
